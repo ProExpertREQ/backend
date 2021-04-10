67aaca38ecf2aad0ee96f10fbdaa1592a830f3d9
Douglas-Castro <farias.douglascastro@gmail.com>
2021-04-09 23:04:51 -0300

    :bulb: Add show, update and delete user

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,9 +4,86 @@ class UserController {
   async store(req, res) {
     try {
       const newUser = await User.create(req.body);
-      res.json(newUser);
+      return res.json(newUser);
     } catch (e) {
-      res.status(400).json({
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+
+      return res.json(users);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+      const user = await User.findByPk(id);
+
+      return res.json(user);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['O ID não foi enviado.'],
+        });
+      }
+
+      const user = await User.findByPk(id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe.'],
+        });
+      }
+
+      const novosDados = await user.update(req.body);
+
+      return res.json(novosDados);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['O ID não foi enviado.'],
+        });
+      }
+
+      const user = await User.findByPk(id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe.'],
+        });
+      }
+
+      await user.destroy();
+
+      return res.json(`O usuário vinculado a matrícula ${user.matricula} foi excluído.`);
+    } catch (e) {
+      return res.status(400).json({
         errors: e.errors.map((err) => err.message),
       });
     }
