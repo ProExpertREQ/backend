43b21a653e8400ac7f2b111fc2a68e837a26a9ac
Douglas-Castro <farias.douglascastro@gmail.com>
2021-04-10 21:37:40 -0300

    :bulb: Add verification of the token data are the same as database data.

--- src/middlewares/loginRequired.js
+++ src/middlewares/loginRequired.js
@@ -1,8 +1,7 @@
 import jwt from 'jsonwebtoken';
+import User from '../models/User';
 
-require('dotenv').config();
-
-export default (req, res, next) => {
+export default async (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization) {
@@ -16,15 +15,26 @@ export default (req, res, next) => {
   try {
     const dados = jwt.verify(token, process.env.TOKEN_SECRET);
 
-    const { matricula, email } = dados;
+    const { id, email } = dados;
 
-    req.userMatricula = matricula;
+    const user = await User.findOne({
+      where: {
+        id,
+        email,
+      },
+    });
+
+    if (!user) {
+      return res.status(401).json({
+        errors: ['Usuário inválido.'],
+      });
+    }
+
+    req.userId = id;
     req.userEmail = email;
 
     return next();
   } catch (e) {
-    console.log(e);
-
     return res.status(401).json({
       errors: ['Token expirado ou inválido.'],
     });
