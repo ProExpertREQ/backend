82cda63d3fe74cbccc619e3839a7b15f4f172345
Douglas-Castro <farias.douglascastro@gmail.com>
2021-04-11 20:25:23 -0300

    :bulb: Add Departamento model in connection database

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,9 @@
 import Sequelize from 'sequelize';
 import databaseConfig from '../config/database';
 import User from '../models/User';
+import Departamento from '../models/Departamento';
 
-const models = [User];
+const models = [User, Departamento];
 
 const connection = new Sequelize(databaseConfig);
 
