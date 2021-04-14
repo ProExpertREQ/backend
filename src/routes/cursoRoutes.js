c6c7beafeddf1b73551d9cae8eb407d935da3a63
Douglas-Castro <farias.douglascastro@gmail.com>
2021-04-13 22:57:17 -0300

    :bulb: Add Curso routes

---
+++ src/routes/cursoRoutes.js
@@ -0,0 +1,8 @@
+// import { Router } from 'express';
+// import cursoController from '../controllers/CursoController';
+
+// const router = new Router();
+
+// router.post('/', cursoController.store);
+
+// export default router;
