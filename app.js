2dc3eaffb8a13a563f47dceec7ebce451736d459
Douglas-Castro <farias.douglascastro@gmail.com>
2021-04-13 22:54:05 -0300

    :bulb: Add cursos routes

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ import homeRoutes from './src/routes/homeRoutes';
 import userRoutes from './src/routes/userRoutes';
 import tokenRoutes from './src/routes/tokenRoutes';
 import departamentoRoutes from './src/routes/departamentoRoutes';
+import cursoRoutes from './src/routes/cursoRoutes';
 
 class App {
   constructor() {
@@ -27,6 +28,7 @@ class App {
     this.app.use('/users/', userRoutes);
     this.app.use('/tokens/', tokenRoutes);
     this.app.use('/departamentos/', departamentoRoutes);
+    // this.app.use('/cursos/', cursoRoutes);
   }
 }
 
