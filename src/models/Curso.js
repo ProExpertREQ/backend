ff2722b4d92a9d3b38ec07fd6aeef03ec08a7b73
Douglas-Castro <farias.douglascastro@gmail.com>
2021-04-13 22:56:57 -0300

    :bulb: Add Curso Model

---
+++ src/models/Curso.js
@@ -0,0 +1,30 @@
+import Sequelize, { Model } from 'sequelize';
+
+export default class Curso extends Model {
+  static init(sequelize) {
+    super.init({
+      nome: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        unique: {
+          msg: 'Esse curso já foi cadastrado.',
+        },
+        validate: {
+          len: {
+            args: [5, 255],
+            msg: 'A campo nome precisa ter entre 5 e 255 caracteres.',
+          },
+        },
+      },
+    },
+    {
+      sequelize,
+    });
+
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.Departamento, { foreignKey: 'departamento_id', as: 'possuiCurso' });
+  }
+}
