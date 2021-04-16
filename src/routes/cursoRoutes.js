1cd27bc1bf2e1ab687fd2463258eaeff8fc5b151
Douglas-Castro <farias.douglascastro@gmail.com>
2021-04-16 00:00:05 -0300

    :bulb: Add course delete route

--- src/routes/cursoRoutes.js
+++ src/routes/cursoRoutes.js
@@ -7,6 +7,7 @@ router.post('/departamentos/:departamento_id/cursos/create', cursoController.cre
 router.get('/cursos/get-all', cursoController.getAll);
 router.get('/cursos/:id', cursoController.getCourseById);
 router.put('/cursos/:id/update', cursoController.update);
+router.delete('/cursos/:id/delete', cursoController.delete);
 router.get('/departamentos/:departamento_id/cursos', cursoController.getCoursesByDepartament);
 
 export default router;
