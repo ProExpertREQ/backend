bf44d6a0bc57240b67b7f4ecd57b7268d815943e
Douglas-Castro <farias.douglascastro@gmail.com>
2021-04-15 23:15:47 -0300

    :bulb: Add get courses by departament

--- src/routes/cursoRoutes.js
+++ src/routes/cursoRoutes.js
@@ -5,5 +5,6 @@ const router = new Router();
 
 router.post('/departamentos/:departamento_id/cursos/create', cursoController.create);
 router.get('/cursos/get-all', cursoController.getAll);
+router.get('/departamentos/:departamento_id/cursos', cursoController.getCoursesByDepartament);
 
 export default router;
