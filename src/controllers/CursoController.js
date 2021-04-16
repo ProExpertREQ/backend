fb81d108f1394b15745e2d023b1f1f0aa06d05f2
Douglas-Castro <farias.douglascastro@gmail.com>
2021-04-15 23:13:47 -0300

    :bulb: Add get courses by departament

--- src/controllers/CursoController.js
+++ src/controllers/CursoController.js
@@ -17,7 +17,9 @@ class CursoController {
 
       const curso = await Curso.create({ departamento_id, nome });
 
-      return res.json(curso);
+      const dados = { departamento: departamento.nome, curso_id: curso.id, nome: curso.nome };
+
+      return res.json(dados);
     } catch (e) {
       return res.status(400).json({
         errors: e.errors.map((err) => err.message),
@@ -27,9 +29,36 @@ class CursoController {
 
   async getAll(req, res) {
     try {
-      const cursos = await Curso.findAll({ attributes: ['departamento_id', 'nome'] });
+      const cursos = await Curso.findAll({ attributes: ['id', 'nome'] });
 
       return res.json(cursos);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async getCoursesByDepartament(req, res) {
+    try {
+      const { departamento_id } = req.params;
+
+      const departamento = await Departamento.findByPk(departamento_id);
+
+      if (!departamento) {
+        return res.status(400).json({
+          errors: 'O departamento não existe.',
+        });
+      }
+
+      const cursos = await Curso.findAll({
+        where: {
+          departamento_id,
+        },
+        attributes: ['id', 'nome'],
+      });
+
+      return res.json({ departamento: departamento.nome, cursos });
     } catch (e) {
       return res.status(400).json({
         errors: e.errors.map((err) => err.message),
