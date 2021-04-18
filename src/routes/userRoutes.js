0def4ea3216145dce505eb38ca5c854c95bae4c5
Douglas-Castro <farias.douglascastro@gmail.com>
2021-04-18 02:09:21 -0300

    :bulb: Add password change route

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,11 +5,12 @@ import userController from '../controllers/UserController';
 const router = new Router();
 
 router.get('/get-all', userController.getAll);
-router.get('/:id', loginRequired, userController.getUserById);
+router.get('/', loginRequired, userController.getUserById);
 
 router.post('/create', userController.create);
 
 router.put('/update', loginRequired, userController.update);
+router.put('/password-change', loginRequired, userController.changePassword);
 router.delete('/delete', loginRequired, userController.delete);
 
 export default router;
