426cce5f00712045d159a22cc056b999afb64ee5
Douglas-Castro <farias.douglascastro@gmail.com>
2021-05-03 23:28:48 -0300

    :bulb: Add getDisciplinesById

--- src/controllers/DisciplinaController.js
+++ src/controllers/DisciplinaController.js
@@ -70,6 +70,84 @@ class DisciplinaController {
       });
     }
   }
+
+  async getDisciplineById(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['O ID da disciplina não foi enviado.'],
+        });
+      }
+
+      const disciplina = await Disciplina.findByPk(id);
+
+      if (!disciplina) {
+        return res.status(400).json({
+          errors: ['Essa disciplina não existe.'],
+        });
+      }
+
+      const { curso_id, nome } = disciplina;
+
+      return res.json({ curso_id, id, nome });
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  // async update(req, res) {
+  //   try {
+  //     if (!req.params.id) {
+  //       return res.status(400).json({
+  //         errors: ['O ID do curso não foi encontrado.'],
+  //       });
+  //     }
+
+  //     const curso = await Curso.findByPk(req.params.id);
+
+  //     if (!curso) {
+  //       return res.status(400).json({
+  //         errors: ['O curso procurado não existe.'],
+  //       });
+  //     }
+
+  //     const { id, nome } = await curso.update(req.body);
+
+  //     return res.json({ id, nome });
+  //   } catch (e) {
+  //     return res.status(400).json({
+  //       errors: e.errors.map((err) => err.message),
+  //     });
+  //   }
+  // }
+
+  // async delete(req, res) {
+  //   try {
+  //     if (!req.params.id) {
+  //       return res.status(400).json({
+  //         errors: ['O ID do curso não foi enviado.'],
+  //       });
+  //     }
+
+  //     const curso = await Curso.findByPk(req.params.id);
+
+  //     if (!curso) {
+  //       return res.status(400).json({
+  //         errors: ['O curso não existe.'],
+  //       });
+  //     }
+
+  //     await curso.destroy();
+
+  //     return res.json(`O curso '${curso.id} ${curso.nome}' foi deletado.`);
+  //   } catch (e) {
+  //     return res.status(400).json({
+  //       errors: e.errors.map((err) => err.message),
+  //     });
+  //   }
+  // }
 }
 
 export default new DisciplinaController();
--- src/routes/disciplinaRoutes.js
+++ src/routes/disciplinaRoutes.js
@@ -4,7 +4,8 @@ import disciplinaController from '../controllers/DisciplinaController';
 const router = new Router();
 
 router.get('/get-all', disciplinaController.getAll);
-router.get('/:curso_id', disciplinaController.getDisciplinesByCourse);
+// router.get('/:curso_id', disciplinaController.getDisciplinesByCourse);
+router.get('/:id', disciplinaController.getDisciplineById);
 router.post('/:curso_id/create', disciplinaController.create);
 // router.put('/update/:id',);
 // router.delete('/delete/:id',);
