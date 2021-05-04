8fbeab0c5d9fe89dc477667577917e08bb1e5bdb
Douglas-Castro <farias.douglascastro@gmail.com>
2021-05-04 00:41:46 -0300

    :bug: Fix route's name

--- src/routes/disciplinaRoutes.js
+++ src/routes/disciplinaRoutes.js
@@ -3,10 +3,10 @@ import disciplinaController from '../controllers/DisciplinaController';
 
 const router = new Router();
 
-router.get('/disciplinas/get-all', disciplinaController.getAll);
 router.get('/cursos/:curso_id/disciplinas', disciplinaController.getDisciplinesByCourse);
+router.post('/cursos/:curso_id/disciplinas/create', disciplinaController.create);
+router.get('/disciplinas/get-all', disciplinaController.getAll);
 router.get('/disciplinas/:id', disciplinaController.getDisciplineById);
-router.post('/disciplinas/:curso_id/create', disciplinaController.create);
 router.put('/disciplinas/update/:id', disciplinaController.update);
 router.delete('/disciplinas/delete/:id', disciplinaController.delete);
 
