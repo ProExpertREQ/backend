ad3197a85c8516d7f65a18eea235ce6b46e040e6
Douglas-Castro <farias.douglascastro@gmail.com>
2021-05-03 23:08:19 -0300

    :bulb: Add hasDisciplines associate

--- src/database/index.js
+++ src/database/index.js
@@ -3,11 +3,13 @@ import databaseConfig from '../config/database';
 import User from '../models/User';
 import Departamento from '../models/Departamento';
 import Curso from '../models/Curso';
+import Disciplina from '../models/Disciplina';
 
-const models = [User, Departamento, Curso];
+const models = [User, Departamento, Curso, Disciplina];
 
 const connection = new Sequelize(databaseConfig);
 
 models.forEach((model) => model.init(connection));
 
 Curso.associate(connection.models);
+Disciplina.associate(connection.models);
