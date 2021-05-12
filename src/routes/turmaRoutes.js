43081ea73ff9b9c1420bd35e5c9f91df2c83b895
Douglas-Castro <farias.douglascastro@gmail.com>
2021-05-12 18:57:28 -0300

    :arrow_up: Update Turmas routes, add Turmas register route and loginRequired middlewares

--- src/routes/turmaRoutes.js
+++ src/routes/turmaRoutes.js
@@ -1,13 +1,16 @@
 import { Router } from 'express';
 import turmaController from '../controllers/TurmaController';
+import loginRequired from '../middlewares/loginRequired';
 
 const router = new Router();
 
-router.get('/turmas/get-all', turmaController.getAll);
+router.get('/turmas/get-all', loginRequired, turmaController.getAll);
 router.get('/disciplinas/:disciplina_id/turmas', turmaController.getClassesByDiscipline);
-router.get('/turmas/:id', turmaController.getClassById);
-router.post('/disciplinas/:disciplina_id/turmas/create', turmaController.create);
-router.put('/turmas/update/:id', turmaController.update);
-router.delete('/turmas/delete/:id', turmaController.delete);
+router.get('/turmas/:id', loginRequired, turmaController.getClassById);
+router.post('/disciplinas/:disciplina_id/turmas/create', loginRequired, turmaController.create);
+router.put('/turmas/update/:id', loginRequired, turmaController.update);
+router.delete('/turmas/delete/:id', loginRequired, turmaController.delete);
+
+router.post('/turmas/:id/register', loginRequired, turmaController.register);
 
 export default router;
