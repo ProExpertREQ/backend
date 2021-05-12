282c5810a22879931097fc950181b8ffa87004d7
Douglas-Castro <farias.douglascastro@gmail.com>
2021-05-12 18:58:06 -0300

    :arrow_up: Update DisciplinasCursadas model

--- src/models/DisciplinasCursadas.js
+++ src/models/DisciplinasCursadas.js
@@ -1,13 +1,23 @@
-import { Model } from 'sequelize';
+import Sequelize, { Model } from 'sequelize';
 
 export default class DisciplinasCursadas extends Model {
   static init(sequelize) {
-    super.init({}, { sequelize });
+    super.init({
+      faltas: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        defaultValue: 0,
+      },
+      presencas: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        defaultValue: 0,
+      },
+    }, { sequelize });
 
     return this;
   }
 
   static associate(models) {
-    this.belongsTo(models.Disciplina, { foreignKey: 'disciplina_id', as: 'hasClasses' });
+    this.belongsTo(models.Turma, { foreignKey: 'turma_id', as: 'hasClasses' });
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'hasSubjects' });
   }
 }
