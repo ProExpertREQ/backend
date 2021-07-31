2ef04fd232e75ca1eadd39569ade80bde546baec
Douglas-Castro <farias.douglascastro@gmail.com>
2021-07-30 22:35:14 -0300

    :arrow_up: Update routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,12 +4,10 @@ import userController from '../controllers/UserController';
 
 const router = new Router();
 
-router.get('/get-all', userController.getAll);
-router.get('/', loginRequired, userController.getUserById);
-
 router.post('/create', userController.create);
 router.post('/login', userController.login);
 
+router.get('/', loginRequired, userController.getUserById);
 router.put('/update', loginRequired, userController.update);
 router.put('/password-change', loginRequired, userController.changePassword);
 router.delete('/delete', loginRequired, userController.delete);
